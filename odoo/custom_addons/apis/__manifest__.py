bc5be5be58993b234baf7561610f1e8a336e0cca
abdelrhmanGoudaa <Abdelrahman.gouda@strategizeit.us>
2024-06-25 14:50:59 +0300

    geo enabled app

---
+++ odoo/custom_addons/apis/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'user Apis',
+    'version': '15.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base',
+        'hr_attendance'
+    ],
+    'auto_install': False,
+    'application': False,
+
+}
